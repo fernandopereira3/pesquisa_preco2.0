ab445540ffee70b31aebc16fd4c36a952bacb759
Fernando <fernandopereira-3@hotmail.com>
2023-10-16 17:50:54 -0300

    first commit

---
+++ wscrapy.py
@@ -0,0 +1,36 @@
+import csv
+import datetime
+import requests
+import parsel
+
+def produto_1():  # IMAC M1
+    req = requests.get("https://www.zoom.com.br/search?q=imac%20m1")
+    sel = parsel.Selector(text=req.text)
+    title = sel.xpath(
+        '//h2[@class="Text_Text__h_AF6 Text_MobileLabelXs__ER_cD Text_DesktopLabelSAtLarge__baj_g ProductCard_ProductCard_Name__LT7hv"][1]//text()').get()
+    price = sel.xpath('//p[@class="Text_Text__h_AF6 Text_MobileHeadingS__Zxam2"]//text()').get()
+    return [title, price]
+
+def produto_2():  # TV 65"
+    req = requests.get(
+        "https://www.zoom.com.br/search?q=tv%2065%20polegadas%204k&hitsPerPage=24&page=1&sortBy=price_asc&isDealsPage=false&enableRefinementsSuggestions=true")
+    sel = parsel.Selector(text=req.text)
+    title = sel.xpath(
+        '//h2[@class="Text_Text__h_AF6 Text_MobileLabelXs__ER_cD Text_DesktopLabelSAtLarge__baj_g ProductCard_ProductCard_Name__LT7hv"][1]//text()').get()
+    price = sel.xpath('//p[@class="Text_Text__h_AF6 Text_MobileHeadingS__Zxam2"]//text()').get()
+    link = sel.xpath(
+        '//h3[@class="Text_Text__h_AF6 Text_MobileLabelXs__ER_cD Text_MobileLabelSAtLarge__YdYbv ProductCard_ProductCard_BestMerchant__AuGU6"]//text()').get()
+    return [title, price, link]
+
+
+def organizador():
+    pro1 = produto_1()
+    pro2 = produto_2()
+    now = datetime.datetime.now()
+    time = now.strftime("%H:%M -- %d/%m/%Y")
+    dado = open('dados.csv', 'w', newline='', encoding='utf-8')
+    escrita = csv.writer(dado)
+    escrita.writerow([pro2[0], pro2[1]])
+    dado.close
+
+    
