574eea27b632a63fa637ea099194a5becf0ec519
Fernando Pereira <fernandopereira-3@hotmail.com>
2024-11-05 15:20:59 -0300

    update 1 para processador

--- wscrapy.py
+++ wscrapy.py
@@ -6,28 +6,30 @@ import smtplib
 import email.message
 import schedule
 
-def produto_1():  # IMAC M1
-    req = requests.get("https://www.zoom.com.br/search?q=imac%20m1")
-    sel = parsel.Selector(text=req.text)
-    title = sel.xpath('//*[@id="product-card-12484406::name"]//text()').get()
-    price = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/p[1]//text()').get()
-    link = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/h3//text()').get()
-    return title, price, link
+#def produto_1():  # IMAC M1
+    #req = requests.get("https://www.zoom.com.br/search?q=imac%20m1")
+    #sel = parsel.Selector(text=req.text)
+    #title = sel.xpath('//*[@id="product-card-12484406::name"]//text()').get()
+    #price = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/p[1]//text()').get()
+    #link = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/h3//text()').get()
+    #return title, price, link
 
 def produto_2():  # TV 65"
+    #blocoValores > div.sc-4f698d6c-0.hkfkrb
+    #main-content > main
     try:
-        req = requests.get("https://www.zoom.com.br/search?q=tv%2065%20polegadas%204k&hitsPerPage=24&page=1&sortBy=price_asc&isDealsPage=false&enableRefinementsSuggestions=true", headers={'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'})
+        req = requests.get("https://www.kabum.com.br/produto/378412/processador-amd-ryzen-9-7900x-5-6ghz-max-turbo-cache-76mb-am5-12-nucleos-video-integrado-100-100000589wof", headers={'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'})
         sel = parsel.Selector(text=req.text)
-        title = sel.xpath('//*[@id="product-card-12484406::name"]//text()').get()
-        price = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/p[1]//text()').get()
-        link = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/h3//text()').get()
-        return title, price, link
+        title = sel.xpath('//*[@id="main-container::name"]//text()').get()
+        #price = sel.xpath('//*[@id="class"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/p[1]//text()').get()
+        #link = sel.xpath('//*[@id="__next"]/div/div[7]/div[1]/a/div[2]/div[2]/div[2]/h3//text()').get()
+        return title
     except Exception as e:
         print(f"Error fetching product 2: {e}")
 
 
 def log():
-    pro1 = produto_1()
+    #pro1 = produto_1()
     pro2 = produto_2()
     now = datetime.datetime.now()
     time = now.strftime("%d/%m/%Y as %H:%M ")
@@ -60,10 +62,10 @@ def enviar():
     except Exception as e:
         print(f"Error sending email: {e}")
 
-schedule.every(15).minutes.do(log)
-schedule.every(30).minutes.do(enviar)
+schedule.every(1).minutes.do(log)
+#schedule.every(30).minutes.do(enviar)
 
 while True:
     schedule.run_pending()
-    time.sleep(120)
+    #time.sleep(120)
 
